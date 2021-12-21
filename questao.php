039c2020f7bcb7f1dacc4e731a14e10115347146
tales <talessilvaamaral@gmail.com>
2021-12-21 16:08:54 -0300

    atualizando

--- questao.php
+++ questao.php
@@ -34,48 +34,48 @@
 				<a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 			</div>
 		</nav>
-        <?php
-                $questao = $_GET['questao'];
+<?php
+$questao = $_GET['questao'];
 
-                $servername = "localhost";
-                $username = "root";
-                $password = "usbw";
-                $database = "base";
+$servername = "localhost";
+$username = "root";
+$password = "usbw";
+$database = "baseresolv";
 
-                // Create connection
-                $conn = mysqli_connect($servername, $username, $password,$database);
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$database);
+mysqli_set_charset($conn,"utf8");
+$sql = "SELECT * FROM questao where fk_Disciplina_ID_Disciplina = $questao";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	$tales = 1;
+	while($row = $result->fetch_assoc()) {
+		$enunciado = $row["Enunciado"];
+	}
+} else {
+	$tales=0;
+	echo "0 results";
+}
+$conn->close();
 
-                $sql = "SELECT * FROM questao where ID = $questao";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                  $tales = 1;
-                  while($row = $result->fetch_assoc()) {
-                    $enunciado = $row["Enunciado"];
-                  }
-                } else {
-                  $tales=0;
-                  echo "0 results";
-                }
-                $conn->close();
+?>
 
-        ?>
+	<section class="container center-container">
+	    <section class="section_content">
+	    <p><b><?php 
+if($tales==1){
+	echo $enunciado;
+}
 
-        <section class="container center-container">
-            <section class="section_content">
-                    <p><b><?php 
-                    if($tales==1){
-                      echo $enunciado;
-                    }
-                    
-                    ?></b></p>
-					          <br />
-            </section>
-        </section>
+?></b></p>
+						  <br />
+	    </section>
+	</section>
       <!--JavaScript at end of body for optimized loading-->
       <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
     </body>
   </html>
-        
+
 
