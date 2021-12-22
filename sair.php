5131d6363b6546d9555b4a47e62c568107fd3603
lucdnitz <lucas.nitz@hotmail.com>
2021-12-22 14:36:05 -0300

    sair

--- sair.php
+++ sair.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $_SESSION['login']=null;
-$_SESSION['idUsuarioSessao']==null;
+$_SESSION['idUsuarioSessao']=null;
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 $extra = 'index.php';
