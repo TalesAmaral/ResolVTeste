22dd9dfe3521100a021a874fce27e26a1e4707d1
tales <talessilvaamaral@gmail.com>
2021-12-22 09:53:33 -0300

    adicionando o navbar no mobile

--- header.php
+++ header.php
@@ -21,7 +21,15 @@ if(isset($_SESSION['login']) && $_SESSION['login']==True){
       </ul>
 
       <ul id="nav-mobile" class="sidenav">
-	<li><a href="#">Navbar Link</a></li>
+	<li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Questões<i class="material-icons right">arrow_drop_down</i></a></li>
+<?php 
+if(isset($_SESSION['login']) && $_SESSION['login']==True){
+	echo "<li><a href='sair.php'>Sair</a></li>";
+}else{
+	echo"<li><a href='login.php'>Login</a></li>
+		<li><a href='registrar.php'>Cadastrar</a></li>";
+}
+?>
       </ul>
       <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
