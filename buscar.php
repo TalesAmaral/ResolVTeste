03315dbd2c9afaa055bacb4556ea9b9cee6b896a
tales <talessilvaamaral@gmail.com>
2021-12-23 10:47:22 -0300

    voltando com o buscar

--- buscar.php
+++ buscar.php
@@ -19,7 +19,7 @@
 		<?php include 'header.php';?>
 			<form class = "row">
 
-				<div class="input-field col s3">
+				<div class="input-field col s12 m3">
 					<select multiple>
 						<option value="" disabled selected>Escolha as disciplinas</option>
 
@@ -47,7 +47,48 @@
 
 					</select>
 				</div>
+				<div class="input-field col s12 m3">
+					<select multiple>
+						<option value="" disabled selected>Escolha os Vestibulares</option>
 
+						<?php 
+							$servername = "localhost";
+							$username = "root";
+							$password = "usbw";
+							$database = "baseresolv";
+
+							// Create connection
+							$conn = mysqli_connect($servername, $username, $password,$database);
+							mysqli_set_charset($conn,"utf8");
+							$sql = "SELECT Nome FROM Vestibular ORDER BY ID ASC;";
+							$i = 1;
+							$result = $conn->query($sql);
+							if ($result->num_rows > 0) {
+								while($row = $result->fetch_assoc()) {
+									echo "<option value=\"$i\"> {$row["Nome"]}</option>";
+									$i+=1;
+								}
+							}
+
+						?>
+
+
+					</select>
+				</div>
+				<div class="input-field col s6 m2">
+					<label for="ano-comeco"> Escolha o ano inicial:</label>	
+					<input id="ano-comeco" type="number" min="1900" max="2099" step="1" value="1990" />
+				</div>
+
+				<div class="input-field col s6 m2">
+					<label for="ano-fim"> Escolha o ano final:</label>	
+					<input id="ano-fim" type="number" min="1900" max="2099" step="1" value="2016" />
+				</div>
+				<div class="input-field col s12 m2">
+
+					  <textarea id="textarea1" class="materialize-textarea"></textarea>
+					  <label for="textarea1">Pesquisar:</label>
+				</div>
 			</form>
 
 
