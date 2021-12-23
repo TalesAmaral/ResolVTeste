b340c28a39e7103d34928897b9ffc2f1297d431e
lucdnitz <lucas.nitz@hotmail.com>
2021-12-22 23:58:55 -0300

    a

--- cadastrar-questao.php
+++ cadastrar-questao.php
@@ -88,7 +88,7 @@
 
 					<button type="submit" class="login-center btn waves-effect waves-light">Enviar</button>
 					<?php 
-						if(isset($_SESSION['login']) && isset($_POST['enunciado']) && $_POST['alternativaCorreta']!=-1 && $_POST['disciplinas']!=-1){
+						if(isset($_SESSION['login']) && isset($_POST['enunciado']) && isset($_POST['alternativaCorreta']) && isset($_POST['disciplinas']) && is_numeric($_POST['ano'])){
 							$conn = mysqli_connect($servername, $username, $password,$database);
 							mysqli_set_charset($conn,"utf8");
 
